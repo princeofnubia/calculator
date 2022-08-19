d95a207877fe248bd4da6431babf7289a0380bad
Oyewo Abubakri <abubakrilaitan@gmail.com>
2022-08-19 12:08:22 -0500

    final

--- demo.js
+++ demo.js
@@ -15,37 +15,80 @@ class Button {
 class Num extends Button {
   constructor(btn, display) {
     super(btn, display);
-    console.log(this.display);
+  }
+}
+
+class Operator extends Button {
+  constructor(btn, display) {
+    super(btn, display);
+  }
+
+  press() {
+    let currentDisplay = this.display.getDisplay();
+    let lastDisplay = parseInt(currentDisplay[currentDisplay.length - 1]);
+    if (currentDisplay.length === 0) {
+      return;
+    }
+    if (isNaN(lastDisplay)) {
+      return;
+    } else {
+      this.display.display(this.text);
+    }
+    // evaluates percentage and display result
   }
 }
 
 class Clear extends Button {
+  constructor(btn, display, result) {
+    super(btn, display);
+    this.result = result;
+  }
+
+  press() {
+    console.log("I am here");
+    this.display.clear();
+    this.result.clear();
+  }
+}
+
+class Del extends Button {
   constructor(btn, display) {
     super(btn, display);
   }
 
   press() {
     console.log("I am here");
-    this.display.display("");
+    this.display.del();
   }
 }
 
 class Percentage extends Button {
-  constructor() {
-    super();
+  constructor(btn, result, display) {
+    super(btn, result);
   }
 
   press() {
+    let res = parseInt(display.getDisplay()) / 100;
+    if (isNaN(res)) {
+      this.display.display("Error");
+      return;
+    }
+    this.display.display(res);
     // evaluates percentage and display result
   }
 }
 
 class Evaluates extends Button {
-  constructor() {
-    super();
+  constructor(btn, display, result, arithmetic) {
+    super(btn, result);
+    this.display = display;
+    this.result = result;
+    this.arithmetic = arithmetic;
   }
 
   press() {
+    let res = eval(this.display.getDisplay());
+    if (res) this.result.display(res);
     // Evaluates text and display result on the screen
   }
 }
@@ -54,15 +97,29 @@ class Display {
   constructor(screen) {
     this.screen = screen;
   }
-
+  getDisplay() {
+    return this.screen.innerText;
+  }
+  del() {
+    this.screen.innerText = this.screen.innerText.substring(
+      0,
+      this.screen.innerText.length - 1
+    );
+  }
+  clear() {
+    this.screen.innerText = "";
+  }
   display(displayItem) {
-    this.screen.innerText = displayItem;
+    this.screen.innerText += displayItem;
   }
 }
 
-class Arithmetic {
-  static calculate(string) {
-    this.str = string;
+class Result extends Display {
+  constructor(screen) {
+    super(screen);
+  }
+  display(displayItem) {
+    this.screen.innerText = displayItem;
   }
 }
 
@@ -71,13 +128,45 @@ class Arithmetic {
 // they respond accordingly
 
 const display = new Display(document.getElementById("input1"));
-const result = new Display(document.getElementById("input2"));
+const result = new Result(document.getElementById("input2"));
+const buttons = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "decimal",
+];
 
-const buttons1 = document.querySelectorAll(".buttons1");
-const buttons2 = document.querySelectorAll(".buttons2");
+const operators = ["add", "subtract", "multiplication", "division"];
 
-const btn = document.getElementById("three");
-const clear = document.getElementById("AC");
+buttons.forEach((value) => {
+  const btn = new Num(document.getElementById(value), display);
+});
 
-const btn3 = new Num(btn, display);
-const btnclear = new Clear(clear, display);
+operators.forEach((value) => {
+  const btn = new Operator(document.getElementById(value), display);
+});
+
+const btnclear = new Clear(document.getElementById("AC"), display, result);
+const btnDel = new Del(document.getElementById("delete"), display);
+
+// percentage
+const btnPercent = new Percentage(
+  document.getElementById("percent"),
+  result,
+  display
+);
+// equals
+
+const btnEquals = new Evaluates(
+  document.getElementById("equals"),
+  display,
+  result
+);
+//
